5b182e9e76d746c05aa62b3e936b138a22ee0fba
Baalaji <baalajimaestro@pixelexperience.org>
2020-03-14 02:49:41 +0530

    Fix notifier

--- pr_notifier.sh
+++ pr_notifier.sh
@@ -4,6 +4,6 @@ function sendMaintainers() {
     curl -s "https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendmessage" --data "text=${*}&chat_id=-1001287849567&disable_web_page_preview=true&parse_mode=Markdown" > /dev/null
 }
 
-sendMaintainers "\`Xiaomi Vendor Cron%0A%0AI have recieved PR $(cat /tmp/gh_number)\`%0A%0A\`Please look in\` ${ADMINS}%0A%0A[PR Link]($(cat /tmp/gh_url))"
+sendMaintainers "\`Xiaomi Vendor Cron%0A%0AI have recieved PR $SEMAPHORE_GIT_PR_NUMBER\`%0A%0A\`Please look in\` ${ADMINS}%0A%0A[PR Link](https://github.com/PixelExperience-Devices/vendor_xiaomi/pull/$SEMAPHORE_GIT_PR_NUMBER)"
 
 echo "Build Finished!"
