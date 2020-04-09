44e3584176ebc15330a2d0df960ee83a602b6b33
baalajimaestro <baalajimaestro@pixelexperience.org>
2020-04-09 12:30:16 +0530

    Nuke CI notifiers
    Signed-off-by: baalajimaestro <baalajimaestro@pixelexperience.org>

--- pr_notifier.sh
+++
@@ -1,9 +0,0 @@
-ADMINS="@Hlcpereira @baalajimaestro @Shreejoy\_Dash @Dyneteve"
-
-function sendMaintainers() {
-    curl -s "https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendmessage" --data "text=${*}&chat_id=-1001287849567&disable_web_page_preview=true&parse_mode=Markdown" > /dev/null
-}
-
-sendMaintainers "\`Xiaomi Vendor Cron%0A%0AI have recieved PR $SEMAPHORE_GIT_PR_NUMBER\`%0A%0A\`Please look in\` ${ADMINS}%0A%0A[PR Link](https://github.com/PixelExperience-Devices/vendor_xiaomi/pull/$SEMAPHORE_GIT_PR_NUMBER)"
-
-echo "Build Finished!"
